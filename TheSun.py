509441ddfa50310fbc250cd197fa9e80f54a8ba6
Raj Yadav <rajyadav64553@gmail.com>
2026-05-28 09:32:04 +0530

    Add files via upload

---
+++ TheSun.py
@@ -0,0 +1,7 @@
+#Code for Poem -The Sun
+print("Poem - The sun")
+print("")
+print("""The golden sun shines bright and clear,
+It warms the earth throughout the year.
+It rises in the morning light,
+And sets to say a sweet goodnight""")
